7f9950a571f5b4c48dda579a4149bdedb99c130b
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-09-15 08:07:33 -0700

    ui and crud

---
+++ web/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import Home from './components/home/home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+const App = () => <Home />
+
+
+export default App;
